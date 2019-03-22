ce7a351b781e573e53e2141b768d92c612f3ce44
is414 <klynt.yardley@gmail.com>
2019-03-21 19:18:48 -0600

    Adapted for Debian

--- bro_install.sh
+++ bro_install.sh
@@ -2,12 +2,13 @@
 #Script to install bro (aka zeek)
 
 sudo apt-get install cmake make gcc g++ flex bison libpcap-dev libssl-dev python-dev swig zlib1g-dev
-wget https://www.zeek.org/downloads/bro-2.6.1.tar.gz -P /root/Downloads/
-tar -xzvf /root/Dowloads/bro-2.6.1.tar.gz
-cd /root/Downloads/bro-2.6.1/
-/root/Downloads/bro-2.6.1/configure
-make
-sudo make install
-export PATH=$PATH:/usr/local/bro/bin
-rm -rd /root/Downloads/bro-2.6.1/
-rm /root/Dowloads/bro-2.6.1.tar.gz
+mkdir /tmp/bro_install
+wget https://www.zeek.org/downloads/bro-2.6.1.tar.gz -P /tmp/bro_install/
+tar -xzvf /tmp/bro_install/bro-2.6.1.tar.gz -C /tmp/bro_install/
+#cd /tmp/bro_install/bro-2.6.1/
+#/tmp/bro_install/bro-2.6.1/configure
+#make
+#sudo make install
+#export PATH=$PATH:/usr/local/bro/bin
+#rm -rd /tmp/bro_install/bro-2.6.1/
+#rm /tmp/bro_install/bro-2.6.1.tar.gz
